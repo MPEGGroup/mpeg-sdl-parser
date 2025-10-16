cb232bbbb891a324166b92174783332263e02ad6
vectronic <hello.vectronic@gmail.com>
2025-10-16 18:34:00 +0100

    feat!: use custom node props and move style props into parser layer

--- src/util/nodeUtils.ts
+++ src/util/nodeUtils.ts
@@ -1,32 +1,8 @@
-import { NodeType } from "@lezer/common";
-import {
-  AlignmentBitCount,
-  BinaryLiteral,
-  DecimalLiteral,
-  FloatingPointLiteral,
-  HexadecimalLiteral,
-  Identifier,
-  IntegerLiteral,
-} from "../lezer/parser.terms.ts";
 import type { Token } from "../ast/token/Token.ts";
 import type { AbstractNode } from "../ast/node/AbstractNode.ts";
-
-const primitiveNodeTypes = new Set([
-  AlignmentBitCount,
-  BinaryLiteral,
-  DecimalLiteral,
-  FloatingPointLiteral,
-  HexadecimalLiteral,
-  Identifier,
-  IntegerLiteral,
-]);
 
 export function isAbstractNode(
   node: AbstractNode | Token,
 ): node is AbstractNode {
-  return node && typeof node === "object" && "nodeKind" in node;
-}
-
-export function isPrimitiveNodeType(nodeType: NodeType): boolean {
-  return primitiveNodeTypes.has(nodeType.id);
+  return node && (typeof node === "object") && ("nodeKind" in node);
 }
